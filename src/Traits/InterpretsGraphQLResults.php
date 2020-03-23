81122f471c7cc274c14466bf2035b80e6baf89e8
Wouter van den Brink <git@woutervdb.com>
2020-03-23 02:26:14 +0100

    Add GraphQL error message assertion

---
+++ src/Traits/InterpretsGraphQLResults.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Knevelina\LighthouseTest\Traits;
+
+use Illuminate\Foundation\Testing\TestCase;
+use Knevelina\LighthouseTest\Constraints\GraphQLErrorMessageConstraint;
+
+trait InterpretsGraphQLResults
+{
+    public function assertHasGraphQLErrorMessage($response, string $message)
+    {
+        /** @var TestCase $this */
+        static::assertThat($response->json('errors'), new GraphQLErrorMessageConstraint($message));
+    }
+}
